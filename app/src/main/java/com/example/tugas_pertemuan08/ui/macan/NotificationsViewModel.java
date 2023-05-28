9df791617ae4e976f5b303a508d6134aa33ecdb5
Muhammad Dila <mochammeddila21@gmail.com>
2023-05-28 20:15:30 +0700

    Tugas Pertemuan 8

--- app/src/main/java/com/example/tugas_pertemuan08/ui/macan/NotificationsViewModel.java
+++ app/src/main/java/com/example/tugas_pertemuan08/ui/macan/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Macan");
+        mText.setValue("Macan Tutul");
     }
 
     public LiveData<String> getText() {
